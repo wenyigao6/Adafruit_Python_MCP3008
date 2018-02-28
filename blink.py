479a0fbdeb5c60bb96c451c8375eb9ff85551616
wenyigao6 <wenyigao@usc.edu>
2018-02-27 17:45:52 -0800

    added :

--- blink.py
+++ blink.py
@@ -2,6 +2,9 @@ import RPi.GPIO as GPIO
 import time
 
 LedPin = 11  # pin11
+GPIO.setmode(GPIO.BCM)
+
+
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
@@ -19,12 +22,26 @@ def destroy():
 	GPIO.output(LedPin, GPIO.LOW)
 	GPIO.cleanup()
 
+def RCtime (RCpin):
+	reading = 0
+	GPIO.setup(RCpin, GPIO.OUT)
+	GPIO.output(RCpin, GPIO.LOW)
+	time.sleep(0.1)
+
+	GPIO.setup(RCpin, GPIO.IN)
+	# This takes about 1 millisecond per loop cycle
+	while (GPIO.input(RCpin) == GPIO.LOW):
+    	reading += 1
+	return reading
+
 if __name__ == '__main__':
 	setup()
-	try:
-		for i in range(5):
-			blink(0.5)
-		[blink(1) for i in range(4)]
+	# t = True
+	while True
+		try:	
+			[blink(0.5) for i in range(5)]
+			[print(RCtime(18)) for i in range(50)]
+			[blink(1) for i in range(4)]
 
-	except KeyboardInterrupt:
-		destroy()
+		except KeyboardInterrupt:
+			destroy()
