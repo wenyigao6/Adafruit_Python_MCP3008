dcb30dd8e4cd3e212fa811271e7ea93da4c636fb
wenyigao6 <wenyigao@usc.edu>
2018-02-27 17:16:47 -0800

    blink

---
+++ blink.py
@@ -0,0 +1,27 @@
+import RPI.GPIO as GPIO
+import time
+
+LedPin = 11  # pin11
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(LedPin, GPIO.OUT)
+	GPIO.output(LedPin, GPIO.HIGH)
+
+def blink():
+	while True:
+		GPIO.output(LedPin, GPIO.HIGH)
+		time.sleep(1)
+		GPIO.output(LedPin, GPIO.LOW)
+		time.sleep(1)
+
+def destroy():
+	GPIO.output(LedPin, GPIO.LOW)
+	GPIO.cleanup()
+
+if __name__ == '__main__':
+	setup()
+	try:
+		blink()
+	except KeyboardInterrupt:
+		destroy()
